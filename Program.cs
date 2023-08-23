0abe73a1ef8ad4493ea86782a08ef9794300edfb
lightlight98 <light0425@gmail.com>
2023-08-23 15:40:58 +0900

    asd

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.Reflection.Emit;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
 
 namespace RtanWorld
 {
     internal class Program
     {
-        private static Character player;
-       
+
+        public static Character player;
 
         static void Main(string[] args)
         {
@@ -15,21 +17,21 @@ namespace RtanWorld
         }
         static void CaracterInfo()
         {
+            
+
             playerInfo();
             Console.Clear();
             Console.WriteLine("상태보기");
             Console.WriteLine("캐릭터의 정보가 표시됩니다.\n\n\n\n");
             Console.WriteLine("레벨 : {0}", player.Level);
             Console.WriteLine("닉네임 : {0}({1})",player.Name,player.Job);
-            Console.WriteLine("공격력 : {0}",player.Atk);
-            Console.WriteLine("방어력 : {0}",player.Def );
+            Console.WriteLine("공격력 : {0}(+{1})", player.Atk+player.AddAtk, player.AddAtk);
+            Console.WriteLine("방어력 : {0}(+{1})", player.Def+player.AddDef, player.AddDef);
             Console.WriteLine("체력 : {0}",player.Hp);
             Console.WriteLine("소지골드 : {0}",player.Gold ); 
             Console.WriteLine("\n\n0. 나가기\n\n\n");
             Console.WriteLine("원하시는 행동을 입력해주세요");
             Input();
-
-
         }
         static void Input()
         {
@@ -43,23 +45,20 @@ namespace RtanWorld
                 }
                 else if (number == 2)
                 {
-                    Inventory();
+                    
                 }
 
-                else if (number == 0) 
+                else if (number == 0)
                 {
                     gameStart();
-                }                               
+                }
             }
         }
-        static void Inventory() 
-        {
-            Console.Clear();
-        }
+
         static void playerInfo()
         {
             
-            player = new Character("LEE", "궁수", 10, 20, 5, 100, 1000);
+            player = new Character("Name", "전사", 10, 20, 5, 100, 1000, 0, 0);
         }
         static void gameStart()
         {
@@ -78,7 +77,7 @@ namespace RtanWorld
     }
     public class Character
     {
-        public string Name { get; }
+        public string Name { get;  }
         public string Job { get; }
         public int Level { get; }
         public int Atk { get; }
@@ -86,7 +85,11 @@ namespace RtanWorld
         public int Hp { get; }
         public int Gold { get; }
 
-        public Character(string name, string job, int level, int atk,int def, int hp, int gold) 
+        public int AddAtk { get; }
+        public int AddDef { get; }
+
+
+        public Character(string name, string job, int level, int atk,int def, int hp, int gold, int addAtk, int addDef) 
         {
             Name = name;
             Job = job;
@@ -95,9 +98,19 @@ namespace RtanWorld
             Def = def;
             Hp = hp;
             Gold = gold;
+            AddAtk = addAtk;
+            AddDef = addDef;
 
         }
         
     }
-
+  
+    public class Inventory 
+    {
+       
+    }
+    public class Item
+    {
+ 
+    }
 }
